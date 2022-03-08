96661524912db643ba0047df7e9fae515153a0ff
abhishek147singh <83961045+abhishek147singh@users.noreply.github.com>
2022-03-08 07:51:26 -0800

    Add files via upload
    this is for showcase purpose only.

---
+++ main.js
@@ -0,0 +1,22 @@
+const canvas = document.getElementById("myCanvas");
+canvas.width = 400;
+
+const ctx = canvas.getContext("2d");
+const road = new Road(canvas.width / 2 , canvas.width * 0.9 , 5);
+const car = new Car(road.getLaneCenter(2) , 100 , 30 , 50);
+
+animate();
+
+function animate(){
+    car.update();
+    
+    canvas.height = window.innerHeight;
+    ctx.save();
+    ctx.translate(0  , -car.y + canvas.height * 0.7);
+
+    road.draw(ctx);
+    car.draw(ctx);
+
+    ctx.restore();
+    requestAnimationFrame(animate);
+}
